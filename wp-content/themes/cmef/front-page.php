cf3fe2f2ff1a5fc92eb07bf3fabdd45dc8b764f6
Jason Walker <jason@pica.is>
2014-07-30 15:11:36 -0400

    Masonry Layout Should Go In Homepage Work.

--- wp-content/themes/cmef/front-page.php
+++ wp-content/themes/cmef/front-page.php
@@ -2,21 +2,14 @@
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<h1><?php the_title(); ?></h1>
 		<?php //the_content(); ?>
-		<h2>H2 Heading</h2>
-		<h3>H3 Heading</h3>
-		<b>Bold</b><i>Italic</i>
-		<p>Paragraph text. Donec sed odio dui. Cras justo odio, dapibus ac facilisis in. Egestas eget quam. Maecenas faucibus mollis interdum maecenas faucibus. Cras mattis consectetur purus sit amet.</p>
-		<a href="">Like Style</a>
-		<ul>
-			<li>Unordered List</li>
-			<li>Unordered List</li>
-		</ul>
-
-		<ol>
-			<li>Ordered List</li>
-			<li>Ordered List</li>
-		</ol>
-
+		<div class="container-twelve masonry projects">
+			<div class="project four columns alpha omega">column 1</div>
+			<div class="project four columns alpha omega">column 2 <br>column</div>
+			<div class="project four columns alpha omega">column 3</div>
+			<div class="project four columns alpha omega">column 1</div>
+			<div class="project four columns alpha omega">column 2 </div>
+			<div class="project four columns alpha omega">column 3</div>
+		</div>
 	<?php endwhile; ?>
 	<?php endif; ?>
 <?php get_footer(); ?>
