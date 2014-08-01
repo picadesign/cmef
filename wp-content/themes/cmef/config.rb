817c5918b1c5821e7d68c0a06724f5cea5baac58
Jason Walker <jason@pica.is>
2014-08-01 13:31:53 -0400

    Added a breadcrumb plugin. Tried to use the gumbo framework. I'm to far into the project to switch now. For future references. USE THE GUMBY FRAMEWORK! @todo: The page template still need work. For now moving on to the project view.

--- wp-content/themes/cmef/config.rb
+++ wp-content/themes/cmef/config.rb
@@ -1,3 +1,9 @@
+# Tell compass where to find local extensions
+# If you followed directions and ran 'gem install modular-scale' comment the next two lines out:
+extensions_dir = "sass/assets/gumby/extensions"
+
+Compass::Frameworks.register('modular-scale', :path => File.expand_path("#{extensions_dir}/modular-scale"))
+
 #Project Settings
 environment = :development
 project_type = :stand_alone
--- wp-content/themes/cmef/page.php
+++ wp-content/themes/cmef/page.php
@@ -1,9 +1,32 @@
 <?php get_header(); ?>
+	<div class="white-background page box-shadow container-sixteen">
+		<div class="breadcrumbs">
+		    <?php if(function_exists('bcn_display'))
+		    {
+		        bcn_display();
+		    }?>
+		</div>
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-			<h1 class="no-margin"><?php the_title(); ?></h1>
-			<?php the_content(); ?>
-			
+		
+			<h2 class="title"><?php the_title(); ?></h2>
+			<div class="two-thirds column content border-right alpha omega">
+				<?php the_content(); ?>
+			</div>
+			<div class="one-third column omega sidebar">
+				<ul>
+					<li>Page</li>
+					<li>Page</li>
+					<li>Page</li>
+					<li>Page</li>
+					<li>Page</li>
+					<li>Page</li>
+					<li>Page</li>
+					<li>Page</li>
+					<li>Page</li>
+					<li>Page</li>
+				</ul>
+			</div>
 		<?php endwhile; ?>
 		<?php endif; ?>
+	</div>
 <?php get_footer(); ?>
