1b86b4188eee553c5ec7be57fa52f3c1af1ba04e
Jason Walker <jason@pica.is>
2014-08-03 11:12:33 -0400

    Fixed the grid system. Updated the program view.

--- wp-content/themes/cmef/single-program.php
+++ wp-content/themes/cmef/single-program.php
@@ -1,12 +1,18 @@
 <?php get_header(); ?>
 	<div class="white-background page program box-shadow container-twelve">
-		<div class="breadcrumbs">
+		<div class="breadcrumbs twelve columns alpha omega">
 		    <?php if(function_exists('bcn_display'))
 		    {
 		        bcn_display();
 		    }?>
 		</div>
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<div class="four offset-by-eight columns alpha omega">
+					<a href="" class="button green alignright"><span class="donate-button">Donate to this program</span></a>
+				<?php if(is_user_logged_in()) : ?>
+					<a class="button green alignright button-margin"><span>Edit</span></a>
+				<?php endif; ?>
+			</div>
 			<h2 class="title eight columns alpha"><?php the_title(); ?></h2>
 			<div class="four columns omega">
 				<span class="caps">Share this Project:</span>
@@ -22,10 +28,21 @@
 			<div class="clear"></div>
 			<hr>
 			<div class="four columns alpha program-image">
-				<?php the_post_thumbnail( $size="medium", $attr ); ?>
+				<div class="thumbnail"><?php the_post_thumbnail( $size="medium", $attr ); ?></div>
+				<div class="clear"></div>
+				<div class="twelve columns alpha omega thumbnails">
+					<a href=""><img src="http://placekitten.com/g/66/66" alt=""></a>
+					<a href=""><img src="http://placekitten.com/g/66/66" alt=""></a>
+					<a href=""><img src="http://placekitten.com/g/66/66" alt=""></a>
+					<a href=""><img src="http://placekitten.com/g/66/66" alt=""></a>
+					<a href=""><img src="http://placekitten.com/g/66/66" alt=""></a>
+					<a href=""><img src="http://placekitten.com/g/66/66" alt=""></a>
+					<a href=""><img src="http://placekitten.com/g/66/66" alt=""></a>
+					<div class="twelve columns alpha omega"><a href="">View All Images</a></div>
+				</div>
 			</div>
 			<div class="eight columns quick-stats omega">
-				<div class="twelve columns omega">
+				<div class="twelve columns omega alpha">
 					<div class="five columns alpha">
 					<span class="caps">Fundraising Progress:</span>
 					</div>
@@ -36,7 +53,7 @@
 					</div>
 				</div>
 				<div class="clear"></div>
-				<div class="twelve columns omega">
+				<div class="twelve columns omega alpha row">
 					<table>
 						<tr>
 							<td>Type of Program:</td>
@@ -62,6 +79,36 @@
 							<td>Number of Students:</td>
 							<td>120</td>
 						</tr>
+					</table>
+				</div>
+				<div class="clear"></div>
+				<div class="twelve columns alpha omega row">
+					<hr>
+					<table>
+						<thead>
+							<tr>
+								<td><b>Name</b></td>
+								<td><b>Email</b></td>
+								<td><b>Phone Number</b></td>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Somebody Special</td>
+								<td>somebody@email.com</td>
+								<td>207-564-4556</td>
+							</tr>
+							<tr>
+								<td>Somebody Special</td>
+								<td>somebody@email.com</td>
+								<td>207-564-4556</td>
+							</tr>
+							<tr>
+								<td>Somebody Special</td>
+								<td>somebody@email.com</td>
+								<td>207-564-4556</td>
+							</tr>
+						</tbody>
 					</table>
 				</div>
 			</div>
